ae939b4678ceebf9c0caf0448e7a8b930ac99a9a
wrenl561 <wrenleibing@gmail.com>
2026-04-21 12:22:21 +0100

    adding jquery to form
    not currently functional

--- venueBooking.js
+++ venueBooking.js
@@ -5,13 +5,39 @@ function validateForm() {
     var details = document.getElementById('details');
 
     if (name.value == "") {
-        alert("is empty");
+        $(#name).addClass("error");
+        alert("name is empty");
     }
-}
+    else {
+        $(#name).removeClass("error");
+    };
+    if (email.value == "") {
+        $(#email).addClass("error");
+        alert("email is empty");
+    }
+    else {
+        $(#email).removeClass("error");
+    };
+    if (date.value == "") {
+        $(#date).addClass("error");
+        alert("date is empty");
+    }
+    else {
+        $(#date).removeClass("error");
+    };
+    if (details.value == "") {
+         $(#details).addClass("error");
+        alert("details is empty");
+    }
+    else {
+         $(#details).removeClass("error");
+    };
+};
 
 function init() {
-    document.getElementById('booking').onsubmit = validateForm;
-    
+    //document.getElementById('booking').onsubmit = validateForm;
+    $(#name).addClass("error");
+    alert("test");
 }
 
 window.onload = init;
