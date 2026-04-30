b3534bb2a4da0a95e8aceb307724a14434b1513c
wrenl561 <wrenleibing@gmail.com>
2026-04-30 16:12:45 +0100

    validation flag
    added a boolean flag for validation of venue booking info

--- venueBooking.js
+++ venueBooking.js
@@ -3,31 +3,41 @@ function validateForm() {
     var email = document.getElementById('email');
     var date = document.getElementById('date');
     var details = document.getElementById('details');
+    var validFlag = true;
 
     if (name.value == "") {
         $("#name").addClass("error");
+        validFlag = false;
     }
     else {
         $("#name").removeClass("error");
     };
     if (email.value == "") {
         $("#email").addClass("error");
+        validFlag = false;
     }
     else {
         $("#email").removeClass("error");
     };
     if (date.value == "") {
         $("#date").addClass("error");
+        validFlag = false;
     }
     else {
         $("#date").removeClass("error");
     };
     if (details.value == "") {
          $("#details").addClass("error");
+         validFlag = false;
     }
     else {
          $("#details").removeClass("error");
     };
+
+    if (validFlag) {
+        //add email data etc (create an email address)
+        
+    }
 };
 
 function init() {
